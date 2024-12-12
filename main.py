775e60d5064c2fcfb99493e9d6eb03332ddda661
RisAhamed <riswanahamed38@gmail.com>
2024-12-12 15:57:32 +0530

    Base model created

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.CNN_Classifier import logger
 from src.CNN_Classifier.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.CNN_Classifier.pipeline.base_model_pipeline import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME  = "Data Ingestion Sta~ge"
+STAGE_NAME  = "Data Ingestion Stage"
 
 if __name__ == '__main__':
     try:
@@ -16,3 +17,17 @@ if __name__ == '__main__':
 
 
 
+stage_name = 'Base  Model Preparation'
+
+if __name__ =='__main__':
+    try:
+        logger.info(f">>>>>> stage {stage_name} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========")
+    
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+        
--- src/CNN_Classifier/config/configuration.py
+++ src/CNN_Classifier/config/configuration.py
@@ -1,10 +1,8 @@
 from src.CNN_Classifier.utils.common import *
 from src.CNN_Classifier.constants import params_file_path,config_file_path
 import os
-from box.exceptions import BoxValueError
 from pathlib import Path
-from src.CNN_Classifier.entity.config_entity import DataIngestionConfig
-
+from src.CNN_Classifier.entity.config_entity import  *
 class ConfigurationManager:
     def __init__(self,
                  config_file_path = config_file_path,
@@ -30,6 +28,26 @@ class ConfigurationManager:
             return data_ingestion_config
         except Exception as e:
             raise e
-        except BoxValueError as e:
-            raise e
+        
+   
+    def get_prepare_base_model_config(self)->PrepareBaseModelConfig:
+        config = self.config_file.prepare_base_model
+        params = self.params_file
+        create_directory([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_batch_size=params.BATCH_SIZE,
+            params_learning_rate = params.LEARNING_RATE,
+            params_classes  =params.CLASSES,
+            params_epochs  = params.EPOCHS,
+            params_image_size  = params.IMAGE_SIZE,
+            params_include_top = params.INCLUDE_TOP,
+            params_weights = params.WEIGHTS
+        )
+        return prepare_base_model_config
+
+
     
